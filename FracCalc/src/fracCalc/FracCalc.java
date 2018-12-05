adbda0c9a1bc3ef1df0c7aacbcd7aa120544bf3c
bryanvtam <19tamb45@stu.smuhsd.org>
2018-12-05 10:41:33 -0800

    finished frac calc

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -42,7 +42,6 @@ public class FracCalc {
         int[] frac1 = {numer1,deno1};
         int[] frac2 = {numerator2, denominator2};
         String answer = "";
-        int[] answers = new int[2];
         //SEPERATES OPERAND1 INTO WHOLEN NUMBER, NUMERATOR, AND DENOMINATOR
         if (operand1.contains("_")) {
         	wholeN1 = Integer.parseInt(operand1.split("_")[0]);
@@ -95,119 +94,39 @@ public class FracCalc {
        
        //CHECK AND CHOOSE OPERATIONS
        if (operation.equals("+")){
-    	   answer = (addition(numer1,numerator2,deno1,denominator2));
-    	   answers = addition(frac1, frac2);
+    	   answer = toMixnum(reduce(addition(frac1, frac2)));
        }
        else if(operation.equals("-")) {
-    	   answer = (subtraction(numer1,numerator2,deno1,denominator2));
-    	   answers = subtraction(frac1, frac2);
+    	   answer = toMixnum(reduce(subtraction(frac1, frac2)));
        }
        else if(operation.equals("*")) {
-    	   answer = (multiplication(numer1,numerator2,deno1,denominator2));
-    	   answers = multiplication(frac1,frac2);
+    	   answer = toMixnum(reduce(multiplication(frac1,frac2)));
        }
        else if(operation.equals("/")) {
-    	   answer = (division(numer1,numerator2,deno1,denominator2));
-    	   answers = division(frac1,frac2);
+    	   answer = toMixnum(reduce(division(frac1,frac2)));
        }
         return (answer);
         //return ("whole:"+ wholenum1 + " numerator:" + numerator1 + " denominator:" + denominator1);
     }
-    // TODO: Fill in the space below with any helper methods that you think you will need
-    public static String addition (int numerator1, int numerator2,int denominator1, int denominator2) {
-    	int m1 =(lcm(denominator1,denominator2))/denominator1;
-    	int m2 =(lcm(denominator1,denominator2))/denominator2;
-    	int newnumerator = ((numerator1*m1)+(numerator2*m2));
-    	String answer = toMix(reduce(newnumerator + "/" + (lcm(denominator1,denominator2))));
-    	return answer;
-    }
-    
-    public static String subtraction (int numerator1, int numerator2,int denominator1, int denominator2) {
-    	int m1 =(lcm(denominator1,denominator2))/denominator1;
-    	int m2 =(lcm(denominator1,denominator2))/denominator2;
-    	int newnumerator = ((numerator1*m1)-(numerator2*m2));
-    	String answer = toMix(reduce(newnumerator + "/" + (lcm(denominator1,denominator2))));
-    	return answer;
-    }
-    
-    public static String multiplication (int numerator1, int numerator2, int denominator1, int denominator2) {
-    	int newnumerator = numerator1*numerator2;
-    	int newdenominator = denominator1*denominator2;
-    	String answer = toMix(reduce(newnumerator + "/" + newdenominator));
-    	return answer;
-    }
-    
-    public static String division (int numerator1, int numerator2, int denominator1, int denominator2) {
-    	int newnumerator = numerator1*denominator2;
-    	int newdenominator = denominator1*numerator2;
-    	String answer = toMix(reduce(newnumerator + "/" + newdenominator));
-    	return answer;
-    }
-    
-    public static int lcm(int denominator1, int denominator2) {
-    	int multiply = 1;
-    	if(denominator1 != denominator2) {
-    		while((denominator1*multiply)%denominator2 !=0) {
-    			multiply++;
-    		}
-    	}
-    		return (denominator1*multiply);
-    }
-    
-    public static String reduce(String input) {	
-    	int numerator = (Integer.parseInt(input.split("/")[0]));
-    	int denominator = (Integer.parseInt(input.split("/")[1]));
-    	int gcd = gcd(numerator, denominator);
-    	
-    	return((numerator/gcd) + "/" + (denominator/gcd));
-    }
-    
+   //================================ METHODS USING ARRAYS, GCD STAYS THE SAME =========================================================================
     public static int gcd(int a, int b) {
     	if(b == 0) {
     		return a;
     	}
     	return gcd(b, a%b);
     }
-    
-    public static String toMix (String input) {
-    	int newnumerator = Integer.parseInt(input.split("/")[0]);
-    	int newdenominator = Integer.parseInt(input.split("/")[1]);
-    	int wholenum = (newnumerator/newdenominator);
-    	int remainder = (newnumerator%newdenominator);
-    	if(newdenominator<0) {
-    		newdenominator = newdenominator*(-1);
-    		newnumerator = newnumerator*(-1);
-    	}
-    	if (remainder <0) {
-    		remainder = remainder*(-1);
-    	}
-    	if(wholenum == 0) {
-    		if(remainder == 0) {
-    			return(Integer.toString(wholenum));
-    		}
-    		else {
-    		return(remainder+"/"+newdenominator);
-    		}
-    	}
-    	else {
-    		return ( wholenum + "_" + remainder +"/" + newdenominator);
-    	}
-    }
-    
-   //=========================================================== METHODS USING ARRAYS, GCD STAYS THE SAME =========================================================================
-    
     public static int[] addition (int[] frac1 , int[] frac2) {
     	int[] answer = new int[2];
-    	answer[1]=lcm(frac1[1],frac2[1]);
-    	answer[0]=(frac1[0]*(answer[1]/frac1[1])) + (frac2[0]*(answer[1]/frac2[1]));
+    	answer[0] = (frac1[0] * frac2[1]) + (frac2[0] + frac1[1]);
+    	answer[1] = (frac1[1] * frac2[1]);
 
     	return answer;
     }
     
     public static int[] subtraction(int[] frac1, int[] frac2) {
     	int[] answer = new int[2];
-    	answer[1]=lcm(frac1[1],frac2[1]);
-    	answer[0]=(frac1[0]*(answer[1]/frac1[1])) - (frac2[0]*(answer[1]/frac2[1]));
+    	answer[0] = (frac1[0] * frac2[1])- (frac2[0] * frac1[1]);
+    	answer[1] = (frac1[1] * frac2[1]);
 
     	return answer;
     }
@@ -237,35 +156,34 @@ public class FracCalc {
     public static String toMixnum(int[]reducedfrac) {
     	int numerator = reducedfrac[0];
     	int denominator = reducedfrac[1];
-    	int remainder = (numerator%denominator);
-    	int wholenum = (numerator/denominator);
+    	
+    	if (denominator < 0) {
+    		numerator *= (-1);
+    		denominator *= (-1);
+    	}
+    	
+    	int remainder = numerator % denominator;
+    	int wholenum = numerator/denominator;
     	String answer = "";
-    	if (wholenum == 0) {
-    		if (denominator < 0) {
-    			denominator *= (-1);
-    			numerator *= (-1);
-    			answer = (numerator + "/" + denominator);
-    		}
-    		else if(numerator == 0) {
-    			answer =  "0";
+    	
+    	if(numerator != 0) {
+    		if (wholenum != 0) {
+    			if(remainder == 0 ) {
+    				answer = Integer.toString(wholenum);
+    			}
+    			else if(remainder < 0) {
+    				remainder *= (-1);
+    				answer = (wholenum + "_" + remainder +"/" + denominator);
+    			}
     		}
     		else {
-    			answer = (numerator + "/" + denominator);
+    			answer = (remainder + "/" + denominator);
     		}
     	}
     	else {
-    		if(numerator < 0 ) {
-    			numerator *= (-1);
-    			answer = (wholenum + "_" + numerator + "/" + denominator);
-    		}
-    		else if (denominator < 0) {
-    			denominator *= (-1);
-    			answer = (wholenum + "_" + numerator + "/" + denominator);
-    		}
-    		else if(numerator == 0) {
-    			answer =  Integer.toString(wholenum);
-    		}
-    	}  
+    		answer = "0";
+    	}
+    	
     	return answer;
     }
     
